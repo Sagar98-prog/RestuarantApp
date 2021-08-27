2a8a3ca4b728ea16fdcb1ec206848bd95babcbf1
Sagar98 <vardhavesagar98@gmail.com>
2021-08-27 21:48:55 +0530

    Restuarant App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-// import React from "react";
-// import Restuarant from "./components/Basics/Restuarant";cd 
-// const Restuarant = () => {
-//   return (
-//     <React>
-//     // <div className="card-container">
-
-//     // </div>
-      
-//     </React>
-//   );
-// };
-
-// export default Restuarant;
-// import Restuarant from "./components/Basics/Restuarant";cd 
-// import UseState from "./component/Hooks/useState";
-// import UseEffect from "./component/Hooks/useEffect";
-// import UseReducer from "./components/Hooks/useReducer";
-
-// const App = () => {
-//   return <UseReducer />;
-// };
-
 import React from "react";
 import Restuarant from "./components/Basics/Restuarant";
 // import UseState from "./component/Hooks/useState";
